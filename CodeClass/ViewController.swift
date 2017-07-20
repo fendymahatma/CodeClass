0d5ad348156d928eb0de0b0d1c19b14d1f1d4040
kliklabs indo kreasi <kliklabs@kliklabss-Mac-mini.local>
2017-07-20 16:38:09 +0700

    text value to label

--- CodeClass/ViewController.swift
+++ CodeClass/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelUtama: UILabel!
+    @IBOutlet weak var buttonSubmit: UIButton!
+    @IBOutlet weak var InputText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClick(sender: AnyObject) {
+        let changeLabelTo = InputText.text
+        self.labelUtama.text = changeLabelTo
+    }
 
 }
 
